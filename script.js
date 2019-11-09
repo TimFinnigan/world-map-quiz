094adc396c50ade97662b9ec901b85fa74779fa8
Tim <tim.finnigan1@gmail.com>
2019-11-09 14:21:10 -0500

    switch starting template

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
-$(document).ready(function(){
-    var map = AmCharts.makeChart("chartdiv", {
-        type: "map",
-        theme: "black",
-        fontSize:15,
-        pathToImages:"https://www.amcharts.com/lib/3/images/",
-        addClassNames: true,
-        defs: {
-            filter: [
-                {
-                    id: "blur",
-                    width:"200%",
-                    height:"200%",
-                    x:"-50%",
-                    y:"-50%",
-                    feGaussianBlur: {
-                        in: "SourceGraphic",
-                        stdDeviation: 3
-                    }
-                }
-            ] 
-        },
-        areasSettings: {
-          autoZoom: false,
-          rollOverOutlineColor: "#000000",
-          outlineAlpha: 12,
-          outlineColor: "#fff",
-          outlineThickness: 0.3,
-          color: "#828282",
-          rollOverColor: "#022154",
-          unlistedAreasAlpha: 0.2,
-          unlistedAreasOutlineAlpha: 0.2,
-          unlistedAreasOutlineColor: "#000000"
-        },
-        linesSettings: {
-          color: "#000000",
-          thickness: 2
-        },
-        dataProvider: {
-          zoomLevel:1.2,
-          map: "worldLow",
-          getAreasFromMap: true
-        },
-        zoomControl: {}
-      });
+$(document).ready(function() {
+  var map = AmCharts.makeChart("mapdiv", {
+    type: "map",
+    theme: "dark",
+    projection: "mercator",
+    panEventsEnabled: true,
+    backgroundColor: "#535364",
+    backgroundAlpha: 1,
+    zoomControl: {
+      zoomControlEnabled: true
+    },
+    dataProvider: {
+      map: "worldHigh",
+      getAreasFromMap: true,
+      areas: []
+    },
+    areasSettings: {
+      autoZoom: true,
+      color: "#B4B4B7",
+      colorSolid: "#84ADE9",
+      selectedColor: "#84ADE9",
+      outlineColor: "#666666",
+      rollOverColor: "#9EC2F7",
+      rollOverOutlineColor: "#000000"
+    }
   });
+});
